68c6e2d90036232e79a24729e4262e06ddbc51fb
Pgc150 <90201294+Pgc150@users.noreply.github.com>
2024-11-22 14:03:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+
+const apikey = 'fbe7a4bc809c4efb8f1c02f94030d962';
+
+const blogContainer = document.getElementById("blog-container");
+const searchField = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+async function fetchRandomNews(){
+    try{
+         const apiUrl  = `https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=10&apiKey=${apikey}`;
+         const response = await fetch(apiUrl);
+
+         // check id response is ik(status 200-299)
+
+         if(!response.ok){
+            throw new Error(`HTTP error! Ststus:${response.status}`)
+         }
+         const data =  await response.json();
+        //  
+        return data.articles || [];
+
+        }catch(error){ 
+        console.error("Error fetching random news",error);
+        return[];
+    }
+}
+
+// taking user input for search
+searchButton.addEventListener("click" ,async ()=>{
+    const query = searchField.value.trim()
+    if(query !== ""){
+        try{
+            const articles = await fetchNewsQuery
+            (query);
+            displayBlogs(articles);
+        }catch(error){
+            console.log("Error fetching news by query",error);
+        }
+    }
+})
+
+async function fetchNewsQuery(query){
+    try{
+        const apiUrl  = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apikey}`;
+        const response = await fetch(apiUrl);
+
+        // check id response is ik(status 200-299)
+
+        if(!response.ok){
+           throw new Error(`HTTP error! Ststus:${response.status}`)
+        }
+        const data =  await response.json();
+       //  
+       return data.articles || [];
+
+       }catch(error){ 
+       console.error("Error fetching random news",error);
+       return[];
+   }
+
+}
+
+function displayBlogs(articles){
+    blogContainer.innerHTML = ""; // clear previous content
+    articles.forEach((article) => {
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+
+        const img  = document.createElement("img");
+        img.src = article.urlToImage || "palceholder-image-url.jpg"; //add fallback image
+        img.alt = article.title;
+
+        const title = document.createElement("h2");
+        const truncatedTitle = article.title.length >30 ? article.title.slice(0,30) + "...." : article.title;
+        title.textContent = truncatedTitle;
+
+        const description = document.createElement("p");
+        const truncatedDescription = article.description.length >120 ? article.description.slice(0,1200) + "...." : article.description;
+        description.textContent = truncatedDescription;
+        
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogCard.addEventListener("click",()=>{
+              window.open(article.url,"_blank");
+        });
+        blogContainer.appendChild(blogCard);
+        
+    });
+}
+
+// Immediately Invoked Function Expression for async handling
+(async () =>{
+    try{
+       const articles = await fetchRandomNews();
+       displayBlogs(articles);
+    }catch(error){
+       console.error("Error fetching random news",error);
+    }
+})();
+
+// const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fbe7a4bc809c4efb8f1c02f94030d962';
+// async function fetchRandomNews() {
+//     const response = await fetch(url);
+//     const data = response.json();
+//     console.log( data.description);
+// }
+
+// fetchRandomNews();
